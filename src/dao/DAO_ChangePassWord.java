24079e8e614ef28c4f6ae1e5d72bb492f8577060
unknown <viet.forwork.2501@gmail.com>
2021-10-21 22:44:17 +0700

    done changePassword

--- src/dao/DAO_ChangePassWord.java
+++ src/dao/DAO_ChangePassWord.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -15,19 +16,40 @@ public class DAO_ChangePassWord {
 		dsnv=new ArrayList<Nhanvien>();
 	}
       
-      public static boolean changePass(Nhanvien nv) {
+      public boolean changePass(Nhanvien nv) {
     	  Connection conn= ConnectDatabase.getConnection();
     	  int n = 0;
     	  try { 
-    	  String sql="update nhanvien set matkhau=? where ma_nv=?";
+    	  String sql="update [dbo].[nhanvien] set matkhau=? where ma_nv=?";
 		  PreparedStatement ps=conn.prepareStatement(sql);
 		  ps.setString(1, nv.getMatkhau());
+		  ps.setString(2,nv.getMaNV());
 		  n=ps.executeUpdate();
 		  
 		} catch (SQLException e) {
 			// TODO: handle exception
-			e.printStackTrace();
+			System.err.println(e);
 		}
     	  return n>0;
+      }
+      public boolean checkLogin(String manv,String matkhau){
+      	Connection conn=ConnectDatabase.getConnection();
+      	int n=0;
+      	try {
+  			String sql="select * from nhanvien where ma_nv=? and matkhau=?";
+  			PreparedStatement ps=conn.prepareStatement(sql);
+  			ps.setString(1, manv);
+  			ps.setString(2, matkhau);
+  			ResultSet rs=ps.executeQuery();
+  			if(rs.next()) {
+  				return true;
+  			}
+  		} catch (SQLException e) {
+  			// TODO: handle exception
+  			System.err.println(e);
+  			return false;
+  		//	e.printStackTrace();
+  		}
+      	return n>0;
       }
 }
